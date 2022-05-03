b290f511ada3770571ab9f4e9f78e3e881847cc3
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-03 23:04:41 +0530

    🌱 Variables

--- Javascript/Day-01.js
+++ Javascript/Day-01.js
@@ -25,7 +25,67 @@ console.timeEnd('Hello');
   line
   comments
 */
+
 // Variables in Js
 
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+var firstName = 'John'; // Camel case
+var first_name = 'Sara'; // Underscore
+var FirstName = 'Tom'; // Pascal case
+var firstname;
+
+// LET
+let name;
+name = 'John Doe';
+console.log(name);
+
+name = 'Steve Smith';
+console.log(name);
+
+// Let have a block level scope 
+{
+    let name = 'Amir' // This variable is accessible in this block only
+    console.log(name);
+}
+
+
+// CONST
+const name = 'John';
+console.log(name);
+// Can not reassign
+name = 'Sara';
+Have to assign a value
+const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);  --> Will return updated class
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
 
 
