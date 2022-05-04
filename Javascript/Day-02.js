85555d06f9c489ad3fdabb1f29c7187cbdb6cee1
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-04 11:26:17 +0530

    To String function

---
+++ Javascript/Day-02.js
@@ -0,0 +1,16 @@
+// Type conversion & Type coercion
+
+let myVar = 12;
+console.log(myVar);
+
+// To String
+// ----------
+
+console.log(String(myVar)); // Number to String
+console.log(String(true)); // Boolean to String
+console.log(String(new Date())); // Date to String
+
+let arr = [1,3,4,5];
+console.log(String(arr)); // Array to String  | enables applying string methods to array
+
+
