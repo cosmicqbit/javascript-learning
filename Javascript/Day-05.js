492e1154a14599e4bc2b21db6407fbde4f2974bd
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-07 10:59:29 +0530

    Crawler Solution

---
+++ Javascript/Day-05.js
@@ -0,0 +1,16 @@
+// Web Crawler Solution 
+
+let targetVar = 'javascript';
+let get = document.links;
+let href;
+
+Array.from(get).forEach(function(element) {
+    href = element.href;
+    if(href.includes(targetVar)) {
+        console.log(href);
+    }
+})
+
+
+
+ 
