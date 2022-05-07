fc44f111df1383bb15336720605635f9ffdaf4bc
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-07 20:03:31 +0530

    Mathematical operations

--- Javascript/Day-05.js
+++ Javascript/Day-05.js
@@ -11,3 +11,14 @@ Array.from(get).forEach(function(element) {
     }
 })
 
+
+// Math Object
+
+let x = 3;
+let y = 4;
+let z;
+
+z = x + y; // 7
+z = x - 4; // -1
+z = x / y; // 0.77
+z = x * y; // 23
