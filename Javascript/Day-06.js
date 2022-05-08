38c74a4b17e8e7690ef1ad0b6cd9a2f695863768
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-08 11:18:37 +0530

    Update Dat 06

--- Javascript/Day-06.js
+++ Javascript/Day-06.js
@@ -17,3 +17,29 @@ mosh.changeName("Just Mosh");
 console.log(mosh);
 
 
+// Employee constructor
+
+function Employee(name, salary, experience){
+    this.name = name;
+    this.salary = salary;
+    this.experience = experience;
+}
+
+// Slogan
+
+Employee.prototype.slogan = function(){
+    return `This company is the best. Regards ${this.name}`;
+}
+
+// Creating Object
+
+let mosh = new Employee('Mosh', 450000, 8);
+console.log(mosh);
+
+// Inherting from different constructor
+
+function Programmer(name, salary, experience, language){
+    Employee.call(this, name, salary, experience);
+    this.language = language;
+}
+
