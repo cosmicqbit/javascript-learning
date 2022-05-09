fb208983004e5ee617d0221f3fbcf8ae01453a88
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-09 23:37:25 +0530

    Finish

--- Projects/Library/app.js
+++ Projects/Library/app.js
@@ -23,10 +23,33 @@ Display.prototype.add = function (book) {
     tableBody.innerHTML += uiString;
 }
 
+// Implement the clear function
 Display.prototype.clear = function () {
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
 }
+
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.lenght < 2)
+    {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+// Show function
+Display.prototype.show = function(type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type}" role="alert"> ${displayMessage}</div>`;
+    console.log(message);
+    setTimeout(() => {
+        message.innerHTML = '';
+    }, 2000);
+}
+
 
 // Add submit event listener to libraryForm
 
@@ -49,16 +72,23 @@ function libraryFormSubmit(e) {
     else if (programming.checked) {
         type = programming.value;
     } 
-    else if (cooking.checkout) {
+    else if (cooking.checked) {
         type = cooking.value;
     }
 
     let book = new Book(name, author, type);
 
     let display = new Display();
-    display.add(book);
-    display.clear();
 
+    if(display.validate(book)) {
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added!');
+    } 
+    else {
+        display.show('danger', 'Sorry you can not add this book!');
+    }
+    
     e.preventDefault();
 }
 
