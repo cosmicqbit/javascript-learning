f8991b00411ff200eef54cf782606223c96ec875
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-09 22:53:18 +0530

    Update

--- Projects/Library/app.js
+++ Projects/Library/app.js
@@ -13,7 +13,56 @@ function Display() {
 }
 
 // Add methods to display prototype
+Display.prototype.add = function (book) {
+    tableBody = document.getElementById('tableBody');
+    let uiString = ` <tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                    </tr>`;
+    tableBody.innerHTML += uiString;
+}
+
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+
+// Add submit event listener to libraryForm
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    let type;
+
+    if (fiction.checked) {
+        type = fiction.value;
+    } 
+    else if (programming.checked) {
+        type = programming.value;
+    } 
+    else if (cooking.checkout) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+
+    let display = new Display();
+    display.add(book);
+    display.clear();
+
+    e.preventDefault();
+}
 
 
-// Add submit event listener
+
+
 
