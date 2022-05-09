2d2df0eb6b8562df75971b4d2f8fa15a91a10d7f
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-09 18:49:03 +0530

    Delcare constructor

---
+++ Projects/Library/app.js
@@ -0,0 +1,19 @@
+// Constructor
+
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor
+
+function Display() {
+
+}
+
+// Add methods to display prototype
+
+
+// Add submit event listener
+
