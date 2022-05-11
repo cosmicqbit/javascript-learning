cfdd6c412fe1ed340cf61d69d1c35a8dba65edbe
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-11 11:18:18 +0530

    Update Day 07

--- Javascript/Day-07.js
+++ Javascript/Day-07.js
@@ -29,3 +29,27 @@ getStudents();
 // The above code won't write the new student to the DOM cuz it was delayed by 8 seconds 
 // and the write was performed within 1 milli second.
 
+const students = [
+    {name: 'wired', subject: 'JS'},
+    {name: 'qbit', subject: 'ML'}
+]
+
+function enrollStudent(student, callback) { // new
+    setTimeout(function(){
+        students.push(student);
+        callback(); // New 
+    }, 8000);
+}
+
+function getStudents() {
+    setTimeout(function() {
+        let str = '';
+        students.forEach(function(element){
+            str += `<li> ${student.name} </li>`;
+        });
+        document.getElementById('students').innerHTML = str;
+    }, 1000);
+}
+
+let newStudent = {name:"Sunny", subject:"Python"};
+enrollStudent(newStudent, getStudents); // new
