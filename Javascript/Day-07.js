c41db3b0d8593e7d74c67542964754c713a7f612
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-11 12:17:29 +0530

    Update Day 07

--- Javascript/Day-07.js
+++ Javascript/Day-07.js
@@ -53,3 +53,18 @@ function getStudents() {
 
 newStudent = {name:"Sunny", subject:"Python"};
 enrollStudent(newStudent, getStudents); // new
+
+// Promises 
+
+let myPromise = new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+    
+      myResolve(); // when successful
+      myReject();  // when error
+    });
+    
+    // "Consuming Code" (Must wait for a fulfilled Promise)
+    myPromise.then(
+      function(value) { /* code if successful */ },
+      function(error) { /* code if some error */ }
+    );
