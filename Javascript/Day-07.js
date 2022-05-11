8ff3c82af1e16ead6bc30bc39e1cc0b6796ba7b9
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-11 11:15:15 +0530

    Update Day 07

--- Javascript/Day-07.js
+++ Javascript/Day-07.js
@@ -1,2 +1,31 @@
 // Callback Functions
 
+// Suppose this response is coming from server
+const students = [
+    {name: 'wired', subject: 'JS'},
+    {name: 'qbit', subject: 'ML'}
+]
+
+function enrollStudent(student) {
+    setTimeout(function(){
+        students.push(student);
+    }, 8000);
+}
+
+function getStudents() {
+    setTimeout(function() {
+        let str = '';
+        students.forEach(function(element){
+            str += `<li> ${student.name} </li>`;
+        });
+        document.getElementById('students').innerHTML = str;
+    }, 1000);
+}
+
+let newStudent = {name:"Sunny", subject:"Python"};
+enrollStudent(newStudent);
+getStudents();
+
+// The above code won't write the new student to the DOM cuz it was delayed by 8 seconds 
+// and the write was performed within 1 milli second.
+
