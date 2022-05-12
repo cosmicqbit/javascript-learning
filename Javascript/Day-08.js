3837934168a76153935f9a929be856da565f8783
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-12 10:13:18 +0530

    Start Day 08

---
+++ Javascript/Day-08.js
@@ -0,0 +1,20 @@
+// Arrow Functions
+
+// Regular function 
+
+const printStr = function() {
+    console.log('This is a string');
+}
+
+// Arrow function
+
+const arrowStr = () => {
+    console.log('This is also a string');
+}
+
+// Function returning value
+
+const greet = function() {
+    return 'This is a return value';
+}
+
